74560eb860a03ca21ad686ac6407f1e4e371f015
Laura Wu <laurawu@email.unc.edu>
2022-02-08 20:40:42 -0500

    Almost done changed guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -5,8 +5,9 @@ import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
 const call = args['call'];
 
-if(call != "tail" && call != "heads") {
+if(call != "tails" && call != "heads") {
     console.log("Error: no input");
+    console.log("Usage: node guess-flip --call=[heads|tails]");
 }
 else {
     console.log(flipACoin(call));
