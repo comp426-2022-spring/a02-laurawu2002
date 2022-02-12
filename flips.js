c67c37dbd95acc00a653f8c9cfc5029226825c93
Laura Wu <laurawu@email.unc.edu>
2022-02-11 19:55:13 -0500

    CHECK

--- flips.js
+++ flips.js
@@ -3,10 +3,22 @@ import minimist from "minimist"
 
 //Takes in the second argument from the command line
 const args = minimist(process.argv.slice(2))
-const num = args['number'] || 1;
+const num = args['number'];
 
-//Makes an array of coinFlips;
-const array = coinFlips(num);
-console.log(array);
-//Counts the heads and tails in the array;
-console.log(countFlips(array));
+//If no arguement, set the number to 1.
+if (num == null) {
+    let array = coinFlips(1);
+    console.log(array);
+    if (flip_result == [ 'tails' ]) {
+        console.log("{ tails: 1 }");
+    } else {
+        console.log("{ heads: 1 }");
+    }
+}
+else {
+    //Makes an array of coinFlips;
+    let array = coinFlips(num);
+    console.log(array);
+    //Counts the heads and tails in the array;
+    console.log(countFlips(array));
+}
