865e2d8d8fdebd298812646df1d1728df31a7db2
Laura Wu <laurawu@email.unc.edu>
2022-02-11 20:05:15 -0500

    checkinggg

--- flips.js
+++ flips.js
@@ -5,8 +5,18 @@ import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
 const num = args['number'] || 1;
 
-//Makes an array of coinFlips;
-const array = coinFlips(num);
-console.log(array);
-//Counts the heads and tails in the array;
-console.log(countFlips(array));
+if(num == 1) {
+    const array = coinFlips(1);
+    console.log(array);
+    if(array== [ 'tails' ]) {
+        console.log("{ tails: 1 }");
+    } else {
+        console.log("{ heads: 1 }");
+    }
+} else {
+    //Makes an array of coinFlips;
+    const array = coinFlips(num);
+    console.log(array);
+    //Counts the heads and tails in the array;
+    console.log(countFlips(array));
+}
